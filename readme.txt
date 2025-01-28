eaca9ee71c3e2fed7f285cb18cd3897e514a5ec2
chuansakura <59757162+chuansakura@users.noreply.github.com>
2025-01-28 11:35:07 +0800

    Add files via upload

---
+++ readme.txt
@@ -0,0 +1,12 @@
+This whole project includes the files to implement the 8 different sequences to estimate the motor threshold.
+This file should be implemented in MATLAB.
+
+Do not edit all the MATLAB function files. 
+
+Unzip 'Subjects_25000_reprocessed.zip' in the current folder before use!!!!
+
+I used the sequence name as MATLAB files name to show which method is implemented in the corresponding file,
+including run_ACS.m, run_ACSPI.m, run_ACSA.m, run_ACSAPI.m,run_DCS.m, run_DCSPI.m, run_DCSA.m, run_DCSAPI.m.
+
+In each file, a random patient is picked. 
+Then, this type of sequence is implemented once and the threshold estimation deviation is recorded.
---
+++ run_DCSAPI.m
@@ -0,0 +1,29 @@
+clear all;
+
+step=30; %step number to be implemented
+
+if ~exist('Total_Subject_Count', 'var')
+    Total_Subject_Count = 25e3;    % 25k
+end
+
+file_name = fullfile(sprintf('Subjects_%d_reprocessed.mat', Total_Subject_Count));
+load( file_name, 'Subjects', 'Total_Subject_Count', 'y_thresh');
+
+params.step_number = step;   
+params.y_thresh = y_thresh;
+params.opts = optimset('Display', 'off', 'MaxFunEvals', 500000, 'FunValCheck', 'on', 'MaxIter', 10000, 'TolFun', 1e-6, 'TolX', 1e-8);
+
+note=randi([1, 25000]); %pick a random patient from 1 to 25000
+
+params.subj_parameters = Subjects(note).subj_parameters;
+params.thresh_x = Subjects(note).relative_frequency.p50_lin; 
+
+lb = 0;        
+ub = 1.3;        
+params.start_amplitude = lb + (ub - lb) * rand(1);
+
+rr = StochasticApproximation(params, 39, false, 1, 1, false, false,0.15,0.15);
+% penultimate parameter is stepsize a0, last parameter is c
+
+dev_DCSAPI = rr.abs_err*100;  %threshold estimation deviation record for 30 steps (in %MSO)
+
